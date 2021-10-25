e0c3bda794d27cad6416a433e4cadd1e21a8ff7e
Rashad Mahmudov <mkrashad25@gmail.com>
2021-10-25 12:21:08 +0200

    Complete 10 points from User story

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='break-label'>Break Length</div>
+      <div id='session-label'>Session Length</div>
+      <button id='break-decrement'>Break Decrement</button>
+      <button id='session-decrement'>Session Decrement</button>
+      <button id='break-increment'>Break Increment</button>
+      <button id='session-increment'>Session Increment</button>
+      <div id='break-length'>5</div>
+      <div id='session-length'>25</div>
+      <div id='timer-label'>Session</div>
+      <button id='start_stop'>Start/Stop</button>
+      <button id='reset'>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
