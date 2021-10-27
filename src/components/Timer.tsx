5f2186ff75b320e63125343a38e1ebc05fc639b8
Rashad Mahmudov <mkrashad25@gmail.com>
2021-10-27 16:18:58 +0200

    Complete 20 points from user story

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RootState } from '../store';
 import { useSelector, useDispatch } from 'react-redux';
-import { resetTimer } from '../slices/countSlice';
+import moment from 'moment';
+import { start_stop, reset } from '../slices/countSlice';
 
 const Timer: React.FC = () => {
-  // const sessionLenght = useSelector((state: RootState) => state.session.value);
+  const session = useSelector((state: RootState) => state.count.session);
+  const breakTime = useSelector((state: RootState) => state.count.break);
+  const seconds = useSelector((state: RootState) => state.count.seconds);
+
   const dispatch = useDispatch();
+  const [intervalId, setIntervalId] = useState<number | null>(null);
+
+  const isRunning = intervalId != null;
+  const startTimer = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (isRunning) {
+      window.clearInterval(intervalId);
+      setIntervalId(null);
+      // const value = e.target as HTMLElement;
+      // if (value.innerText === 'Reset') dispatch(reset());
+    } else {
+      const newIntervalId = window.setInterval(
+        () => dispatch(start_stop()),
+        1000
+      );
+      setIntervalId(newIntervalId);
+    }
+  };
+  const formatedMinutes = moment().minutes(session).format('mm');
+  const formatedBreak = moment().minutes(breakTime).format('mm');
+  const formatedSeconds = moment().seconds(seconds).format('ss');
+
   return (
     <div>
-      <div id='timer-label'>Session</div>
-      {/* <div id='time-left'>{sessionLenght}:00</div> */}
-      <button id='start_stop'>Start/Stop</button>
-      <button id='reset' onClick={() => dispatch(resetTimer())}>
+      <div id='timer-label'>{session !== 0 ? 'Session' : 'Break'}</div>
+      <div id='time-left'>
+        {formatedMinutes !== '00' ? formatedMinutes : formatedBreak}:
+        {formatedSeconds}
+      </div>
+      <button id='start_stop' onClick={startTimer}>
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
+      <button id='reset' onClick={() => dispatch(reset())}>
         Reset
       </button>
+      {/* <button id='reset' onClick={(e) => startTimer(e)}>
+        Reset
+      </button> */}
     </div>
   );
 };
--- src/slices/countSlice.tsx
+++ src/slices/countSlice.tsx
@@ -3,11 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export interface CountState {
   break: number;
   session: number;
+  seconds: number;
 }
 
 const initialState: CountState = {
   break: 5,
   session: 25,
+  seconds: 60,
 };
 
 export const countSlice = createSlice({
@@ -18,17 +20,31 @@ export const countSlice = createSlice({
       state.break = state.break === 60 ? state.break : state.break + 1;
     },
     decrementBreak: (state) => {
-      state.break = state.break === 1 ? state.break : state.break - 1;
+      state.break = state.break <= 1 ? state.break : state.break - 1;
     },
     incrementSession: (state) => {
       state.session = state.session === 60 ? state.session : state.session + 1;
     },
     decrementSession: (state) => {
-      state.session = state.session === 1 ? state.session : state.session - 1;
+      state.session = state.session <= 1 ? state.session : state.session - 1;
     },
-    resetTimer: (state) => {
+    start_stop: (state) => {
+      state.seconds = state.seconds > 0 ? state.seconds - 1 : 5;
+      state.session =
+        state.seconds < 1 && state.session !== 0
+          ? state.session - 1
+          : state.session;
+      state.break =
+        state.seconds < 1 && state.session === 0 && state.break !== 0
+          ? state.break - 1
+          : state.break === 0
+          ? 1
+          : state.break;
+    },
+    reset: (state) => {
       state.break = 5;
       state.session = 25;
+      state.seconds = 60;
     },
   },
 });
@@ -38,7 +54,8 @@ export const {
   decrementBreak,
   incrementSession,
   decrementSession,
-  resetTimer,
+  start_stop,
+  reset,
 } = countSlice.actions;
 
 export default countSlice.reducer;
