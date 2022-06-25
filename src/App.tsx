a3b382a3d0a5f8e1be5e06fb51e2d99a717b33a3
Rashad Mahmudov <mkrashad25@gmail.com>
2022-06-25 22:05:31 +0200

    style: App and breaklength component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
+    <div className='app-container'>
       <BreakLength />
       <SessionLength />
       <Timer />
--- src/components/BreakLength.tsx
+++ src/components/BreakLength.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { RootState } from '../store';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrementBreak, incrementBreak } from '../slices/timerSlice';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import './styles/breakLength.css';
 
 const BreakLength: React.FC = () => {
   const breakLength = useSelector(
@@ -13,21 +14,27 @@ const BreakLength: React.FC = () => {
   return (
     <>
       <div id='break-label'>Break Length</div>
-      <div id='break-length'>{breakLength}</div>
-      <button
-        id='break-decrement'
-        aria-label='Decrement break'
-        onClick={() => dispatch(decrementBreak())}
-      >
-        <FontAwesomeIcon icon={faMinus} />
-      </button>
-      <button
-        id='break-increment'
-        aria-label='Increment break'
-        onClick={() => dispatch(incrementBreak())}
-      >
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
+      <div className='break-container'>
+        <button
+          id='break-decrement'
+          className='break-decrement'
+          aria-label='Decrement break'
+          onClick={() => dispatch(decrementBreak())}
+        >
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+        <div id='break-length' className='break-length'>
+          {breakLength}
+        </div>
+        <button
+          id='break-increment'
+          className='break-increment'
+          aria-label='Increment break'
+          onClick={() => dispatch(incrementBreak())}
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
     </>
   );
 };
