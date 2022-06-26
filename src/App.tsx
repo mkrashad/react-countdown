b22b614ab1532238143d4d4dbf9eda64b51aad2e
Rashad Mahmudov <mkrashad25@gmail.com>
2022-06-26 17:08:18 +0200

    Create footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import BreakLength from './components/BreakLength';
 import SessionLength from './components/SessionLength';
 import Timer from './components/Timer';
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className='app-container'>
-      <div className='break-session-container'>
-        <SessionLength />
-        <BreakLength />
+    <>
+      <div className='app-container'>
+        <div className='break-session-container'>
+          <SessionLength />
+          <BreakLength />
+        </div>
+        <Timer />
       </div>
-      <Timer />
-    </div>
+        <footer>
+          <a href='https://github.com/rashad25evans' target='_blank' rel="noreferrer">
+            Coded by Rashad Mahmudov
+          </a>
+        </footer>
+    </>
   );
 };
 
